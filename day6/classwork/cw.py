5f66bedf002b75853274a9d81540563946a40085
karala007 <davit.yaralashvili54@gmail.com>
2025-07-24 00:02:37 +0400

    1

---
+++ day6/classwork/cw.py
@@ -0,0 +1,25 @@
+
+#ლოგიკური ოპერატორები — and და or — გამოიყენება პირობითი გამონათქვამების (conditions) ერთმანეთთან შესადარებლად და ლოგიკური
+#გადაწყვეტილებების მისაღებად. ეს ოპერატორები საშუალებას გვაძლევს დავაკავშიროთ ორი ან მეტი პირობა (condition) და საბოლოო 
+# შედეგი მივიღოთ ჭეშმარიტების (True) ან მცდარობის (False) სახით.
+
+
+#მაგ.
+print(True or False)
+print(False and True)
+
+
+
+#მომხმარეებელს შემოაყვანინეთ სახელი, შეამოწმეთ იგი თუ უდრის თქვენს სახელს, არ დაგავიწყდეთ print ფუნქციის გამოყენება
+#პასუხის დასაბეჭდად.
+# Define your own name
+my_name = "daviti"
+Name = input(" your name: ")
+NAME=Name == my_name
+print(NAME)
+
+#გააკეთეთ 2-2 მაგალითი or და and ოპერატორებზე
+print(True and True)
+print(False and False)
+print(False or True)
+print(False or False)
