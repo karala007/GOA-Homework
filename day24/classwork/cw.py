caf4f59fe19e428c25c68d6182116117ac9c06ac
karala007 <davit.yaralashvili54@gmail.com>
2025-09-21 01:40:37 +0400

    1

---
+++ day24/classwork/cw.py
@@ -0,0 +1,29 @@
+# 1
+names = ["ana", "gio", "davita", "lika", "goga", "lahsa", "sofio"]
+
+print(names[-7])
+print(names[-6]) 
+print(names[-5])
+print(names[-4])
+print(names[-3])
+print(names[-2])
+print(names[-1])
+
+# 2
+name1 = "davita"
+name2 = "genadi"
+name3 = "tiko"
+name4 = "tako"
+name5 = "tatia"
+
+people = [name1, name2, name3, name4, name5]
+
+print(people[-1])
+
+# 3
+
+saxeli = "irakli"
+
+print(saxeli[3])
+
+print(saxeli[-3])
