bb03aa056d6d9007516fee4f1581cb70b795c1c4
karala007 <davit.yaralashvili54@gmail.com>
2026-08-11 14:13:03 +0400

    1

---
+++ day111/homework/hm1.py
@@ -0,0 +1,16 @@
+
+# def decimal_to_binary():
+#     number = int(input("rame: "))
+
+#     if number == 0:
+#         return "0"
+
+#     binary = ""
+#     while number > 0:
+#         binary = str(number % 2) + binary
+#         number = number // 2
+
+#     return binary
+
+# result = decimal_to_binary()
+# print(result)
