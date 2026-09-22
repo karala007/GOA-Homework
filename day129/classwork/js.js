de9744cbf1146aa58f0fc687149fd1d366515bcd
karala007 <davit.yaralashvili54@gmail.com>
2026-09-22 20:54:13 +0400

    1

---
+++ day129/classwork/js.js
@@ -0,0 +1,49 @@
+// let numbers = prompt("enter number")
+
+// switch (true) {
+//     case numbers > 0 && numbers % 2 == 0:
+//         console.log("positive even")
+//         break
+//     case numbers > 0 && numbers % 2 == 1:
+//         console.log("positive odd")
+//         break
+//     case numbers < 0 && numbers % 2 == 0:
+//         console.log("negative even")
+//         break
+//     case numbers < 0 && numbers % 2 == 1:
+//         console.log("negative odd")
+//         break
+//     default:
+//         console.log(0)
+// }
+
+
+
+
+// // =======================================================
+
+// function sayMyInfo() {
+//     let name = "davit"
+//     let gvari = "karalashvili"
+//     let age = 20
+//     let address = "tbilisi"
+
+//     console.log(`saxeli ${name} gvarii ${gvari} asaki ${age} misamarti ${address}`)
+// }
+
+// sayMyInfo()
+// sayMyInfo()
+// sayMyInfo()
+
+
+
+// ==========================================================
+
+
+function info(name, surname, parchusPrice) {
+    console.log(`salami me var ${name} ${surname} da  shesyidvis ar vici rame ari ${parchusPrice}`)
+}
+
+info("davit", "karalashvili", 1500)
+info("goga", "chalauri", 250)
+info("luka", "jobava", 80)
